1130db54bfa25b98fe5c88f0f1a7a55375604be1
rarch <rarcher496@gmail.com>
2015-04-01 09:20:48 -0400

    rightmost

---
+++ rightmost.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""Sample code to read in test cases:
+
+import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    # 'test' represents the test case, do something with it
+    # ...
+    # ...
+
+test_cases.close()
+"""
+
+import sys
+
+def main():
+    lines=[]
+    with open(sys.argv[1], 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        (s,c)=line.rstrip("\n").split(",")
+        print s.rfind(c)
+
+if __name__ == "__main__":
+    main()
