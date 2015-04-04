9795a91eb2a12f7db3433a5d254b173fb4b05713
rarch <rarcher496@gmail.com>
2015-04-04 08:53:24 -0400

    bitpositions

---
+++ easy/bitpositions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""Sample code to read in test cases:
+
+import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    # 'test' represents the test case, do something with it
+    # ...
+    # ...
+
+test_cases.close()
+"""
+
+import sys
+
+def main():
+    lines=[]
+    with open(sys.argv[1], 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        # split by commas
+        (n,bp1,bp2)=map(int,line.rstrip().split(","))
+
+        print str((1&(n>>(bp1-1))) == (1&(n>>(bp2-1)))).lower()
+
+if __name__ == "__main__":
+    main()
