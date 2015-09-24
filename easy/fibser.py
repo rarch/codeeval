786e5d856118ac4a76199922902b498dae1cda42
rarch <rarcher496@gmail.com>
2015-09-24 16:53:39 -0400

    fibser

---
+++ easy/fibser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+known_fib = {0:0, 1:1}
+def fib(n):
+    if n in known_fib:
+        return known_fib[n]
+    result = fib(n-1)+fib(n-2)
+    known_fib[n] = result
+    return result
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    # 'test' represents the test case, do something with it
+    print fib(int(test))
+
+test_cases.close()
