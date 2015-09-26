78c2eba66f8dc5992bd51147f510bb9f6a468110
rarch <rarcher496@gmail.com>
2015-09-26 13:08:47 -0400

    longhand guesswine

--- easy/guesswine.py
+++ easy/guesswine.py
@@ -13,11 +13,23 @@ for test in test_cases:
     # result = ' '.join([word for word in words if
                     # all((ch in word) for ch in chars)])
 
+    # result, occurrences = [], {}
     result = []
+
     # deal with duplicates
     for word in words:
         if all(chars.count(char) <= word.count(char) for char in chars):
             result.append(word)
+
+    # for char in chars:
+    #     if char in occurrences:
+    #         occurrences[char] = occurrences[char] + 1
+    #     else:
+    #         occurrences[char] = 1
+
+    # for word in words:
+    #     if all(occurrences[char] <= word.count(char) for char in chars):
+    #         result.append(word)
 
     print ' '.join(result) if result else False
 
