1b8a56f0b0a3eb1bed63629f786a038d3a7b43a7
rarch <rarcher496@gmail.com>
2015-09-27 17:02:42 -0400

    hex2dec

---
+++ easy/hex2dec.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    if not test.rstrip(): continue
+    # 'test' represents the test case, do something with it
+
+    print int(test,16)
+
+test_cases.close()
