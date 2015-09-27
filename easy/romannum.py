b1915dd82126a2d45b326fc97a00f6a47e13b84b
rarch <rarcher496@gmail.com>
2015-09-26 23:07:07 -0400

    fixed loop

--- easy/romannum.py
+++ easy/romannum.py
@@ -2,17 +2,18 @@
 
 import sys
 
+letters = {1000:'M',900:'CM',500:'D',400:'CD',\
+            100:'C',90:'XC',50:'L',40:'XL',\
+            10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+choices = sorted(letters,lambda x,y:y-x)
+
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
     # ignore test if it is an empty line
     if not test.rstrip(): continue
     # 'test' represents the test case, do something with it
 
-    letters = {1000:'M',900:'CM',500:'D',400:'CD',\
-                100:'C',90:'XC',50:'L',40:'XL',\
-                10:'X',9:'IX',5:'V',4:'IV',1:'I'}
-
-    val,res,choices = int(test),'',sorted(letters,lambda x,y:y-x)
+    val,res = int(test),''
 
     for diff in choices:
         if val == 0:
