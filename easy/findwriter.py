525c76b1006611872190d525c268580bc6888926
rarch <rarcher496@gmail.com>
2015-09-28 18:13:25 -0400

    findwriter and iseven

---
+++ easy/findwriter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    test = test.rstrip()
+    if not test: continue
+    # 'test' represents the test case, do something with it
+    chars,keystring = test.split('|')
+    indices = map(int,keystring.strip().split())
+
+    res = ''.join(chars[index-1] for index in indices)
+    print res
+
+test_cases.close()
---
+++ easy/iseven.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    test = test.rstrip()
+    if not test: continue
+    # 'test' represents the test case, do something with it
+    val=int(test)
+    print 1 if (val/2)*2 == val else 0
+
+test_cases.close()
