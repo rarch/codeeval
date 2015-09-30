9a00ff61f0cb0480dcad8f237b10a26d2e4499f2
rarch <rarcher496@gmail.com>
2015-09-29 22:20:18 -0400

    mincoins

---
+++ med/mincoins.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+coins = [5,3,1]
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    # ignore test if it is an empty line
+    if not test.rstrip(): continue
+    # 'test' represents the test case, do something with it
+    val = int(test)
+
+    count = 0
+    for coin in coins:
+        count,val = count+val/coin,val-coin*(val/coin)
+    print count
+
+test_cases.close()
