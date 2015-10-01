2f34683d020a3c9c9a7d037fd0707335e3b19ba3
rarch <rarcher496@gmail.com>
2015-09-30 21:46:12 -0400

    working brute force

--- med/bats.py
+++ med/bats.py
@@ -12,16 +12,15 @@ def main(filename):
     for line in lines:
         vals = map(int,line.split())
         length,tolerance,bats = vals[0:3]
-        locs = sorted(vals[3:]) if bats and len(vals)>3 else []
-
-        # if number is not between upper and lower of padding,
-        #   you can add it to list, increment count, and add to padding
-        # then print count
         count = 0
+
+        locs = vals[3:] if bats and len(vals)>3 else []
+        # counting up, add location if not withing padding of any others
         for val in xrange(FROM_BLDG,length-FROM_BLDG+1):
             if all([abs(loc-val)>=tolerance for loc in locs]):
                 locs=locs+[val]
                 count += 1
+
         print count
 
 if __name__ == "__main__":
