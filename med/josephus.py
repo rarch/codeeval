9b736f6a680dda6ca98d1a89ca57662d5e317834
rarch <rarcher496@gmail.com>
2015-10-01 12:28:28 -0400

    josephus

---
+++ med/josephus.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+
+def josephus(n,m):
+    people,res = [ind for ind in xrange(n)],[]
+
+    ind,count = 0,len(people)
+    while people:
+        ind = (ind+m-1)%count
+        res,count = res+[people.pop(ind)],count-1
+
+    return res
+
+def main(filename):
+    lines=[]
+    with open(filename) as f_in: # get only nonempty lines
+        lines = [line.strip() for line in f_in if line.rstrip()]
+
+    for line in lines:
+        [n,m] = map(int,line.split(','))
+        res = josephus(n,m)
+        print ' '.join([str(val) for val in res])
+
+if __name__ == "__main__":
+    main(sys.argv[1])
