3415fb16a13a2d6baa91322ee24cebb250be7536
rarch <rarcher496@gmail.com>
2015-10-04 19:07:03 -0400

    email validate BOOOMMM

--- med/emailvalidate.py
+++ med/emailvalidate.py
@@ -3,19 +3,15 @@
 import sys
 import re
 
-# ONLY PARTIAL SOLUTION. WHAT IS WRONG WITH THIS REGEX? WHICH EDGE CASE?
-
 def main(filename):
     lines=[]
     with open(filename) as f_in: # get only nonempty lines
         lines = [line.strip() for line in f_in if line.rstrip()]
 
+    email = re.compile(r"(^\"[^\"]+\"|[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
     for line in lines:
-        email=r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-        if not re.match(email,line):
-            print "false"
-        else:
-            print "true"
+        print "true" if email.match(line) else "false"
 
 if __name__ == "__main__":
     main(sys.argv[1])
