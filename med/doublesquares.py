983e529a887f3b6bb94806ea74a6bcf1edcc8a23
rarch <rarcher496@gmail.com>
2015-10-05 12:42:04 -0400

    double squares

--- med/doublesquares.py
+++ med/doublesquares.py
@@ -9,10 +9,10 @@ def main(filename):
         lines = [line.strip() for line in f_in if line.rstrip()]
 
     for line in lines[1:]:
-        c = int(line)
+        x = int(line)
         count=0
-        for a in xrange(int(sqrt(c))+1):
-            b=sqrt(c-a*a)
+        for a in xrange(int(sqrt(x))+1):
+            b=sqrt(x-a*a)
             if b==int(b): # working with integers
                 if a==b: count+=2 # only enters once, but count twice and div/2
                 else: count+=1 # enters twice (a+b,b+a), so count once, and div/2
