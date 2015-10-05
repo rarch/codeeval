8e0d652b786cd5f2d9e3083f3cb5ad39d04dbf85
rarch <rarcher496@gmail.com>
2015-10-05 17:03:30 -0400

    working commuting engineer. BOOM

--- hard/commutingeng.py
+++ hard/commutingeng.py
@@ -5,57 +5,30 @@ from math import sqrt,pi,sin,cos,acos
 from itertools import permutations
 
 START = '1' # Specified in problem
-EARTH_RAD_MILES = 3959
+EARTH_RAD_KM = 6371.0
+DistDict={}
 
-# def distance(ptA,ptB):  # THIS IS CARTESIAN DISTANCE FOR 2D GRAPHS -- YOU IDIOT --
-#                         # YOU'RE WORKING WITH AN APPROXIMATE SPHERE
-#     return sqrt( (ptB[0]-ptA[0])**2 +
-#                 (ptB[1]-ptA[1])**2 )
-
-def roundDistance(ptA,ptB):
-    global EARTH_RAD_MILES
-    phiA,thetaA=ptA[0]*pi/180.0,ptA[1]*pi/180.0
-    phiB,thetaB=ptB[0]*pi/180.0,ptB[1]*pi/180.0
-    return EARTH_RAD_MILES*acos(
-        sin(phiA)*sin(phiB)*cos(thetaA-thetaB)+cos(phiA)*cos(phiB)
+def distancePhiLambda(phi1,lambda1,phi2,lambda2):
+    global EARTH_RAD_KM
+    return EARTH_RAD_KM*acos(
+        sin(phi1)*sin(phi2)+cos(phi1)*cos(phi2)*cos(abs(lambda2-lambda1))
         )
 
-# DistanceMatrx = None
-# def traverse(lengthPathPairs):
-#     global DistanceMatrx
-#     keys = set(DistanceMatrx.keys())
-#     newpaths=[]
-#     for (length,path) in lengthPathPairs:
-#         rmdr = keys-set(path)
-#         if not rmdr:
-#             return lengthPathPairs
-#         for point in rmdr:
-#             newpaths.append(
-#                 (length+DistanceMatrx[path[-1]][point],
-#                 path+[point])
-#             )
-#     return traverse(newpaths)
-# def shortestPath(locs):
-#     global DistanceMatrx
-#     DistanceMatrx={a[0]:{b[0]:roundDistance(a[1:],b[1:])
-#         for b in locs if b!=a}
-#             for a in locs}
-#     return min(traverse([(0.0,[1])]),key=lambda x:x[0])
-
-def shortestPath_rev(locDict):
+def shortestPath(keys):
     global START
-    pathLength,minPath=sys.maxint,[]
-    points=list(locDict.keys())
-    del points[points.index(START)]
-    print points
-    nPoints = len(points)
-    for traversal in permutations(points):
-        temp = roundDistance(locDict[START],locDict[traversal[0]])
-        for ind in xrange(len(traversal)-1):
-            temp+=roundDistance(locDict[traversal[ind]],
-                            locDict[traversal[ind+1]])
-        if temp<pathLength:
-            pathLength,minPath=temp,[START]+list(traversal)
+    global DistDict
+    keys = [elt for elt in keys if not elt==START]
+    nkeys = len(keys)
+
+    minPath,minD = [],sys.maxint
+
+    for journey in permutations(keys):
+        dist = DistDict[START][journey[0]]
+        for ind in xrange(nkeys-1):
+            dist+=DistDict[journey[ind]][journey[ind+1]]
+        if dist<minD:
+            minD,minPath=dist,[START]+list(journey)
+
     return minPath
 
 def main(filename):
@@ -63,21 +36,30 @@ def main(filename):
     with open(filename) as f_in: # get only nonempty lines
         lines = [line.strip() for line in f_in if line.rstrip()]
 
-    coords=re.compile("^(\d+)[^()]*\((-?\d+\.\d+), (-?\d+\.\d+)\)$")
+    coords=re.compile("^(?P<ind>\d+)[^()]*\((?P<lat>-?\d+\.\d+), (?P<lng>-?\d+\.\d+)\)$")
 
-    locs = {}
+    global DistDict
+    keys,points=[],[]
     for line in lines:
-        temp = re.match(coords,line).groups()
-        locs[temp[0]]=(float(temp[1]),float(temp[2]))
-    traversal=shortestPath_rev(locs)
-    print '\n'.join([val for val in traversal])
+        temp = re.match(coords,line)
+        keys+=[temp.group('ind')]
+        points+=[temp.groups()]
 
-    # locs = []
-    # for line in lines:
-    #     temp = re.match(coords,line).groups()
-    #     locs+=[(int(temp[0]),float(temp[1]),float(temp[2]))]
-    # (dist,path) = shortestPath(locs)
-    # print '\n'.join([str(loc) for loc in path])
+    for ptA in points: # initialize distance dictionary
+        for ptB in points:
+            name1,name2=ptA[0],ptB[0] # names must be different and not entered
+            if (name1!=name2):
+                temp=distancePhiLambda(
+                    float(ptA[1])*pi/180.0,float(ptA[2])*pi/180.0,
+                    float(ptB[1])*pi/180.0,float(ptB[2])*pi/180.0)
+                if not name1 in DistDict:
+                    DistDict[name1] = {}
+                if not name2 in DistDict:
+                    DistDict[name2] = {}
+                DistDict[name1][name2]=DistDict[name2][name1]=temp
+
+    traversal=shortestPath(keys)
+    print '\n'.join([pos for pos in traversal])
 
 if __name__ == "__main__":
     main(sys.argv[1])
