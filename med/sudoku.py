105d32f55802f108ab4a211f3a2ae6087ab5e840
rarch <rarcher496@gmail.com>
2015-10-13 13:36:36 -0400

    sudoku

--- med/sudoku.py
+++ med/sudoku.py
@@ -8,35 +8,77 @@ from math import sqrt
 #  8  9  10 11
 # 12 13  14 15
 
-def checkSubGrids(arr,dim):
-    root=int(sqrt(dim))
-    # DOES NOT WORK. WHY NOT?
-    # return all(val in [arr[col+hrz*root+(row+vrt*root)*dim] for hrz in xrange(root)
-    #   for vrt in xrange(root) for row in xrange(root) for col in xrange(root)]
-    #     for val in xrange(1,dim+1))
+# def testSubGrids(arr,dim):
+#     test = [i for i in xrange(dim*dim)]
+#     for i in xrange(dim):
+#         print test[i*dim:i*dim+dim]
+#     print 'groups'
+#     root = int(sqrt(dim))
+#     for gridH in xrange(root):
+#         for gridV in xrange(root):
+#             print [test[subC+root*gridH+dim*(subR+root*gridV)]
+#                 for subR in xrange(root) for subC in xrange(root)]
 
-    # examine all root*root subgrids shifting over by hrz and down by vrt
-    for hrz in xrange(root):
-      for vrt in xrange(0,dim,root):
-        for val in xrange(1,dim+1):
-            if val not in [arr[col+(row+vrt)*dim+hrz*root]
-              for row in xrange(root) for col in xrange(root)]:
-                return False
+# def checkSubGrids(arr,dim):
+#     # THIS DOESN'T WORK, WHY NOT??
+#     root=int(sqrt(dim))
+#     return all(val in [arr[subC+root*gridH+dim*(subR+root*gridV)]
+#       for subR in xrange(root) for subC in xrange(root)
+#         for gridH in xrange(root) for gridV in xrange(root)]
+#           for val in xrange(1,dim+1))
+#     # for gridH in xrange(root):
+#     #     for gridV in xrange(root):
+#     #         for val in xrange(1,1+dim):
+#     #             if val not in [arr[subC+root*gridH+dim*(subR+root*gridV)]
+#     #                 for subR in xrange(root) for subC in xrange(root)]:
+#     #                     return False
+#     # return True
+
+# def checkCols(arr,dim):
+#     # for col in xrange(dim):
+#     #     print [arr[col+row*dim] for row in xrange(dim)]
+#     return all(val in [arr[col+row*dim] for row in xrange(dim) for col in xrange(dim)]
+#         for val in xrange(1,1+dim))
+
+# def checkRows(arr,dim):
+#     # for row in xrange(0,dim*dim,dim):
+#     #     print arr[row:row+dim]
+#     return all([val in arr[row:row+dim]
+#         for val in xrange(1,1+dim) for row in xrange(0,dim*dim,dim)])
+#     # for row in xrange(0,dim*dim,dim):
+#     #     for val in xrange(1,dim+1):
+#     #         if not val in arr[row:row+dim]:
+#     #             return False
+#     # return True
+
+# subgrids 0-8: grid#/3 = row and grid#%3 =col
+
+def checkSubGrids(grid,dim):
+    if not grid: return False
+    root = int(sqrt(dim))
+    for subNum in xrange(dim):
+        boxR,boxC = subNum/root,subNum%root
+        temp = [elt for subR in grid[boxR*root:boxR*root+root]
+            for elt in subR[boxC*root:boxC*root+root]]
+        if not all(val in temp for val in xrange(1,1+dim)):
+            return False
     return True
 
-def checkCols(arr,dim):
-    return all(val in [arr[col+dim*row] for col in xrange(dim) for row in xrange(dim)]
-        for val in xrange(1,dim+1))
+def checkCols(grid,dim):
+    if not grid: return False
+    return all(val in col for col in zip(*grid) for val in xrange(1,1+dim))
 
-def checkRows(arr,dim):
-    return all([val in arr[row:row+dim]
-        for val in xrange(1,dim+1) for row in xrange(0,dim*dim,dim)])
+def checkRows(grid,dim):
+    if not grid: return False
+    return all(val in row for row in grid for val in xrange(1,1+dim))
 
-    # for row in xrange(0,dim*dim,dim):
-    #     for val in xrange(1,dim+1):
-    #         if not val in arr[row:row+dim]:
-    #             return False
-    # return True
+def gridify(gridlst,dim):
+    if dim*dim!=len(gridlst):
+        return None
+    grid = []
+    for row in xrange(dim):
+        grid.append(gridlst[row*dim:row*dim+dim])
+    return grid
 
 def main(filename):
     lines=[]
@@ -46,8 +88,11 @@ def main(filename):
     for line in lines:
         n,data = line.split(';')
         dim = int(n)
-        grid = [int(elt) for elt in data.split(',')]
+        grid = gridify([int(elt) for elt in data.split(',')],dim)
         print checkRows(grid,dim) and checkCols(grid,dim) and checkSubGrids(grid,dim)
+
+        # print dim*dim==len(grid) and 1==min(grid) and dim==max(grid) and\
+        #  checkRows(grid,dim) and checkCols(grid,dim) and checkSubGrids(grid,dim)
 
 if __name__ == "__main__":
     main(sys.argv[1])
