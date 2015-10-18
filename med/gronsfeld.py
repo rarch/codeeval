e29952dacfa8d8fee53fac9eb40723e8d938054e
rarch <rarcher496@gmail.com>
2015-10-17 22:56:58 -0400

    still partial gronsfeld still

--- med/gronsfeld.py
+++ med/gronsfeld.py
@@ -7,9 +7,10 @@ VOCAB=list(" !\"#$%&'()*+,-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghi
 # WHY NOT WORKING 100%? ONLY SCORE OF 95
 def gronsfeld(msg,keys,reverse=False):
     res=''
-    if reverse: keys=map(lambda x:-x,keys)
-    for ind in xrange(len(msg)):
-        shift=keys[ind%len(keys)]
+    if reverse: keys=map(lambda x:-x,keys) # going bkwd if reverse
+    for ind in xrange(len(msg)): # go through message
+        shift=keys[ind%len(keys)] # shift by the int in keys
+        # find char and shift, loop if exceed length of alphabet
         res+=VOCAB[(VOCAB.index(msg[ind])+shift)%len(VOCAB)]
     return res
 
@@ -20,7 +21,7 @@ def main(filename):
 
     for line in lines:
         [nums,data]=line.split(';')
-        print gronsfeld(data,map(int,nums),True)
+        print gronsfeld(data,map(int,nums),True) # reverse gronsfeld
 
 if __name__ == "__main__":
     main(sys.argv[1])
