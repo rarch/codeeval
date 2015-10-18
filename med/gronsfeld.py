42e63fe540f2d3bb698f541452d3170dd4edae09
rarch <rarcher496@gmail.com>
2015-10-17 22:55:15 -0400

    still partial gronsfeld

--- med/gronsfeld.py
+++ med/gronsfeld.py
@@ -2,16 +2,15 @@
 
 import sys
 
-AlphaBet=list(" !\"#$%&'()*+,-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
-
+VOCAB=list(" !\"#$%&'()*+,-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 
 # WHY NOT WORKING 100%? ONLY SCORE OF 95
-def gronsfeld(text,keys,reverse=False):
+def gronsfeld(msg,keys,reverse=False):
     res=''
     if reverse: keys=map(lambda x:-x,keys)
-    for ind in xrange(len(text)):
+    for ind in xrange(len(msg)):
         shift=keys[ind%len(keys)]
-        res+=AlphaBet[(AlphaBet.index(text[ind])+shift)%len(AlphaBet)]
+        res+=VOCAB[(VOCAB.index(msg[ind])+shift)%len(VOCAB)]
     return res
 
 def main(filename):
@@ -20,8 +19,8 @@ def main(filename):
         lines = [line.strip() for line in f_in if line.rstrip()]
 
     for line in lines:
-        [nums,text]=line.split(';')
-        print gronsfeld(text,map(int,nums),True)
+        [nums,data]=line.split(';')
+        print gronsfeld(data,map(int,nums),True)
 
 if __name__ == "__main__":
     main(sys.argv[1])
