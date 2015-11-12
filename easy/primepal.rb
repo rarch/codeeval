9f530cd1739c04654420dd3d38fc3d0a970df2ae
rarch <rarcher496@gmail.com>
2015-11-12 13:50:49 -0500

    figuring out rb

--- easy/primepal.rb
+++ easy/primepal.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 
 # def eratosthenes(n)
-#   nums = [nil, nil, *2..n]
-#   (2..Math.sqrt(n)).each do |i|
-#     (i**2..n).step(i){|m| nums[m] = nil}  if nums[i]
-#   end
-#   nums.compact
+#     is_prime = [false,false]+[true]*(n-1)
+#     2.upto(Math.sqrt(n).ceil) do |i|
+#       next if !is_prime[i]
+#       (i*i).step(n,i) do |j|
+#         is_prime[j] = false
+#       end
+#     end
+#     (0..n).select{|i| is_prime[i]}
 # end
 
 def isPrime(n)
