6820b86d20fb39170156fda4b5afd42aa3a8ff7f
rarch <rarcher496@gmail.com>
2015-11-12 13:03:36 -0500

    starting ruby

---
+++ easy/fizzbuzz.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def fizzbuzz(f,b,n)
+    (1..n).each do |i|
+        if i%f==0
+            print 'F'
+        end
+        if i%b==0
+            print 'B'
+        end
+        if i%f!=0 && i%b!=0
+            print i
+        end
+        break if i==n
+        print " "
+    end
+    puts
+end
+
+File.open(ARGV[0]).each_line do |line|
+    x,y,n=line.split().map{|d|d.to_i}
+    fizzbuzz(x,y,n)
+end
