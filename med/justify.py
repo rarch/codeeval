9d749440ab1fe5a6ba2f1ba32c51c7ea8d9135e0
rarch <rarcher496@gmail.com>
2015-12-11 19:20:19 -0500

    refactored justify

--- med/justify.py
+++ med/justify.py
@@ -11,12 +11,24 @@ def lessThanWidth(lst):
         line.append(lst.pop(0))
     return [line,lst]
 
+def justifyLines(lines):
+    output=[' '.join(lines.pop())] # last line
+    while lines:
+        temp=lines.pop()
+        length = sum([len(word) for word in temp])+len(temp)-1
+        while length<WIDTH:
+            for ind in xrange(len(temp)-1):
+                temp[ind]=temp[ind]+' '
+                length+=1
+                if length==WIDTH: break
+        output.insert(0,' '.join(temp))
+    return '\n'.join(output)
+
 def main(filename):
     global WIDTH
     lines=[]
     with open(filename) as f_in: # get only nonempty lines
         lines = [line.strip() for line in f_in if line.rstrip()]
-    # raise Exception('\n'.join(lines))
 
     for line in lines:
         words,lstsOfWords = line.split(),[]
@@ -24,18 +36,8 @@ def main(filename):
             [lst,words]=lessThanWidth(words)
             lstsOfWords.append(lst)
 
-        output=[' '.join(lstsOfWords.pop())]
-        while lstsOfWords:
-            temp=lstsOfWords.pop()
-            length = sum([len(word) for word in temp])+len(temp)-1
-            while length<WIDTH:
-                for ind in xrange(len(temp)):
-                    temp[ind]=temp[ind]+' '
-                    length+=1
-                    if length==WIDTH:
-                        break
-            output.insert(0,' '.join(temp))
-        print '\n'.join(output)
+        print justifyLines(lstsOfWords)
 
 if __name__ == "__main__":
     main(sys.argv[1])
+    
