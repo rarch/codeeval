b60685427981600f28428cf977b5107db1df9877
rarch <rarcher496@gmail.com>
2015-12-11 19:04:57 -0500

    justify working at 99.36 percent. That's &%0ing ridiculous! What's the issue?

---
+++ med/justify.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+
+WIDTH=80
+
+def lessThanWidth(lst):
+    global WIDTH
+    line=[lst.pop(0)]
+    while lst and len(line)+len(lst[0])+sum([len(elt) for elt in line])<=WIDTH:
+        line.append(lst.pop(0))
+    return [line,lst]
+
+def main(filename):
+    global WIDTH
+    lines=[]
+    with open(filename) as f_in: # get only nonempty lines
+        lines = [line.strip() for line in f_in if line.rstrip()]
+    # raise Exception('\n'.join(lines))
+
+    for line in lines:
+        words,lstsOfWords = line.split(),[]
+        while words:
+            [lst,words]=lessThanWidth(words)
+            lstsOfWords.append(lst)
+
+        output=[' '.join(lstsOfWords.pop())]
+        while lstsOfWords:
+            temp=lstsOfWords.pop()
+            length = sum([len(word) for word in temp])+len(temp)-1
+            while length<WIDTH:
+                for ind in xrange(len(temp)):
+                    temp[ind]=temp[ind]+' '
+                    length+=1
+                    if length==WIDTH:
+                        break
+            output.insert(0,' '.join(temp))
+        print '\n'.join(output)
+
+if __name__ == "__main__":
+    main(sys.argv[1])
